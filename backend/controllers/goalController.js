ed286336fd992b7f6af15a860825fd2b0de28cd8
Kundan Singh <kundasin591998@outlook.com>
2022-07-11 11:49:15 +0530

    checkpoint 2

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,32 +1,46 @@
+const asyncHandler = require('express-async-handler');
+
 //here I am refactoring the C-R-U-D operations
 
 //@desc GET goal
 //@route GET /api/goals
 //@access Private
-const getGoals = (req, res)=>{
-    res.status(200).json({ message: 'Get Goals' });
-}
+const getGoals = asyncHandler(
+    async (req, res)=>{
+        res.status(200).json({ message: 'Get Goals' });
+    }
+) 
 
 //@desc SET goal
 //@route POST /api/goals
 //@access Private
-const setGoals = (req, res)=>{
-    res.status(200).json({ message: 'Set Goals' });
-}
+const setGoals = asyncHandler(
+    async (req, res)=>{
+        if(!req.body.text){
+            res.status(400)
+            throw new Error('Bad request');
+        }
+        res.status(200).json({ message: 'Set Goals' });
+    }
+) 
 
 //@desc UPDATE goal
 //@route PUT /api/goals/:id
 //@access Private
-const updateGoals = (req, res)=>{
-    res.status(200).json({ message: `Update Goal ${req.params.id}` });
-}
+const updateGoals = asyncHandler(
+    async (req, res)=>{
+        res.status(200).json({ message: `Update Goal ${req.params.id}` });
+    }
+)
 
 //@desc DELETE goal
 //@route DELETE /api/goals/:id
 //@access Private
-const deleteGoals = (req, res)=>{
-    res.status(200).json({ message: `Delete Goal ${req.params.id}` });
-}
+const deleteGoals = asyncHandler(
+    async (req, res)=>{
+        res.status(200).json({ message: `Delete Goal ${req.params.id}` });
+    }
+)
 
 module.exports = {
     getGoals,
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
-//import express, dotenv, 
+//import express, dotenv, errorHandler
 const express = require('express');
 const dotenv = require('dotenv').config();
+const { errorHandler } = require('./middleware/errorMiddleware');
 
 //calling PORT from .env file
 const port = process.env.PORT || 5000;
@@ -8,6 +9,10 @@ const port = process.env.PORT || 5000;
 //initialise express
 const app = express();
 
-app.use('/api/goals', require('./routes/goalRoutes'))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.listen(port, ()=> console.log(`server started on port ${port}`))
+app.use('/api/goals', require('./routes/goalRoutes'));
+app.use(errorHandler);
+
+app.listen(port, ()=> console.log(`server started on port ${port}`));
